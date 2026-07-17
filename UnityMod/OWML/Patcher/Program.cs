cb373f0e960bfd67cbb7e2f456c464854b000dac
milkydelta <88966938+milkydelta@users.noreply.github.com>
2026-07-17 09:40:03 +0100

    Add pre-patcher to OWML
    OWML mods are deployed to a folder in appdata, so my package can't just include the native DLLs in an "OuterWilds_Data" folder. If you want to modify game files (or just drop an extra in), you have to make a console program for OWML to call. I've included the dlls as EmbeddedResources, so the amount of sparse files is low.

---
+++ UnityMod/OWML/Patcher/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace OAT_OWML_Patcher;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string modPath = args.Length > 0 ? args[0] : ".";
+        string gamePath = AppDomain.CurrentDomain.BaseDirectory;
+
+        string gameDataPath = "";
+
+        Console.WriteLine($"Game Path: {gamePath}");
+
+        foreach (string folder in Directory.GetDirectories(gamePath, "Outer*", SearchOption.TopDirectoryOnly))
+        {
+            if (folder.EndsWith("_Data"))
+            {
+                gameDataPath = folder;
+            }
+        }
+
+        if (!File.Exists(gamePath + "lincomm.dll"))
+        {
+            System.Console.WriteLine("Writing Linux communication library");
+            CopyResourceToPath("lincomm.dll.so", gamePath + "lincomm.dll");
+        }
+
+        if (!File.Exists(gameDataPath + "/Plugins/x86_64/OAT_KlakSpout.dll"))
+        {
+            System.Console.WriteLine("Writing KlakSpout Plugin");
+            Directory.CreateDirectory(gameDataPath + "/Plugins/x86_64");
+            CopyResourceToPath("KlakSpout.dll", gameDataPath + "/Plugins/x86_64/OAT_KlakSpout.dll");
+            File.Copy(modPath + "/KLAKSPOUT-DLL-LICENSE.txt", gameDataPath + "/Plugins/x86_64/KLAKSPOUT-DLL-LICENSE.txt");
+        }
+    }
+
+    static void CopyResourceToPath(string resource, string path)
+    {
+        Assembly asm = Assembly.GetExecutingAssembly();
+
+        using (Stream res = asm.GetManifestResourceStream(resource))
+        using (FileStream file = File.Open(path, FileMode.Create))
+        {
+            res.CopyTo(file);
+        }
+    }
+}
