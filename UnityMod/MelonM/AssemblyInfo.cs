5f290ebb09012dc1a19e62086c38a2cb5c9e24c9
milkydelta <88966938+milkydelta@users.noreply.github.com>
2026-08-13 15:03:13 +0100

    bump version

--- UnityMod/MelonM/AssemblyInfo.cs
+++ UnityMod/MelonM/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using MelonLoader;
 using OnAirTap.Melon;
 
-[assembly: MelonInfo(typeof(Mod), "OnAirTap", "1.1.0", "milkydelta")]
+[assembly: MelonInfo(typeof(Mod), "OnAirTap", "1.1.1", "milkydelta")]
 [assembly: MelonGame()]
 [assembly: HarmonyDontPatchAll]
